6e298fc4faab250e2387bc83f3095c8a887531d1
Tanisha-1799 <triveditanisha01171999@gmail.com>
2024-02-05 20:10:57 +0530

    Adding test containers to perform automated ntegration tests

--- src/test/java/com/techyme/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/techyme/productservice/ProductServiceApplicationTests.java
@@ -2,12 +2,17 @@ package com.techyme.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
+
+	MongoDBContainer mongoDBContainer=new MongoDBContainer();
 
 	@Test
 	void contextLoads() {
 	}
-
+	//Using "testcontainers" are used for integration tests, a java library.
 }
